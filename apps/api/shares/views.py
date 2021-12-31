61062065b9c025c855463742fd099cc2f9ae9bc9
oliver <498946975@qq.com>
2021-12-31 21:37:15 +0800

    多文件分享

--- apps/api/shares/views.py
+++ apps/api/shares/views.py
@@ -4,7 +4,7 @@ from utils import token
 from utils.get_share import get_share_random_str
 from db.get_db import get_db
 from utils.docs_operation import *
-from utils.shares_operation import share_db_add
+from utils.shares_operation import share_db_add, share_db_adds
 
 router = APIRouter(
     prefix="/shares"
@@ -68,4 +68,67 @@ def share_add(
         state, access_type, access_number,
         doc_id
     )
+    return {"code": 200, "msg": "分享成功", "share_url": share_url, "share_pwd": sharePwd}
+
+
+@router.post("/share_adds", tags=["文档模块"])
+def share_adds(
+        sharePwd: str = Form(None),
+        share_type: int = Form(...),
+        doc_ids: str = Form(...),
+        access_type: int = Form(...),
+        access_number: int = Form(...),
+        user_id: str = Depends(token.parse_token),
+        db: Session = Depends(get_db)):
+    """
+    多选文件分享
+    :param sharePwd:
+    :param share_type:
+    :param doc_ids:
+    :param access_type:
+    :param access_number:
+    :param user_id:
+    :param db:
+    :return:
+    """
+    doc_ids = doc_ids.split(",")
+    # 生成随机字符串
+    share_url = get_share_random_str(16)
+    # share_pwd进行判断，如果为空则自动生成，否则直接使用
+    if sharePwd == None or sharePwd == "":
+        sharePwd = get_share_random_str(4)
+
+    # todo 根据有效期判断，传开始时间和结束时间
+    end_time = datetime.datetime.now()
+
+    # 访问人数设置
+    if access_type == 1:
+        access_number = 0
+    elif access_type == 2:
+        access_number = access_number
+    else:
+        access_number = 0
+    if share_type != 1:
+        if share_type == 2:
+            now_time = datetime.datetime.now()
+            end_time = now_time + datetime.timedelta(days=30)
+        if share_type == 3:
+            now_time = datetime.datetime.now()
+            end_time = now_time + datetime.timedelta(days=7)
+        if share_type == 4:
+            now_time = datetime.datetime.now()
+            end_time = now_time + datetime.timedelta(days=1)
+    elif share_type == 1:
+        now_time = datetime.datetime.now()
+        end_time = now_time + datetime.timedelta(days=99999)
+
+    state = 1
+    share_db_adds(
+        db, int(user_id),
+        share_url, sharePwd,
+        share_type, end_time,
+        state, access_type, access_number,
+        doc_ids
+    )
+
     return {"code": 200, "msg": "分享成功", "share_url": share_url, "share_pwd": sharePwd}
--- utils/shares_operation.py
+++ utils/shares_operation.py
@@ -15,6 +15,20 @@ def share_db_add(
         access_number: int,
         doc_id: int
 ):
+    """
+    增加单独文件分享
+    :param db:
+    :param user_id:
+    :param share_url:
+    :param share_pwd:
+    :param share_type:
+    :param end_time:
+    :param state:
+    :param access_type:
+    :param access_number:
+    :param doc_id:
+    :return:
+    """
     share = Shares(
         user_id=user_id,
         share_url=share_url,
@@ -25,15 +39,66 @@ def share_db_add(
         access_type=access_type,
         access_number=access_number,
     )
-
+    # 单独文件分享增加到数据库
     db.add(share)
     db.commit()
     db.flush()
-
+    # 分享到id和文件id进行关联
     share_item = ShareItem(
         share_id=share.id,
         doc_id=doc_id
     )
     db.add(share_item)
+    db.commit()
+    db.flush()
+
+
+def share_db_adds(
+        db: Session,
+        user_id: int,
+        share_url: str,
+        share_pwd: str,
+        share_type: int,
+        end_time: datetime.datetime,
+        state: int,
+        access_type: int,
+        access_number: int,
+        doc_ids: [str]
+):
+    """
+    多条分享
+    :param db:
+    :param user_id:
+    :param share_url:
+    :param share_pwd:
+    :param share_type:
+    :param end_time:
+    :param state:
+    :param access_type:
+    :param access_number:
+    :param doc_ids:
+    :return:
+    """
+    share = Shares(
+        user_id=user_id,
+        share_url=share_url,
+        share_pwd=share_pwd,
+        share_type=share_type,
+        end_time=end_time,
+        state=state,
+        access_type=access_type,
+        access_number=access_number,
+    )
+    db.add(share)
+    db.commit()
+    db.flush()
+    # 保存多条分享的映射关系
+    for doc_id in doc_ids:
+        share_item = ShareItem(
+            share_id=share.id,
+            doc_id=doc_id
+        )
+        db.add(share_item)
+
     db.commit()
     db.flush()
