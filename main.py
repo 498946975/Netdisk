d415bd377f71b5fade91b01559a348574cf3b267
oliver <498946975@qq.com>
2022-01-12 15:19:48 +0800

    docker-compose

--- main.py
+++ main.py
@@ -36,4 +36,4 @@ app.include_router(shares_router)
 app.include_router(download_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app="main:app", reload=True, host="127.0.0.1", port=8888)
+    uvicorn.run(app="main:app", reload=True, host="0.0.0.0", port=8888)
